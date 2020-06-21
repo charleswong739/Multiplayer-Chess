3beecd89946a6f98223c92be55a54768ad40e1ea
TLiu-8 <66648833+TLiu-8@users.noreply.github.com>
2020-06-21 18:07:19 -0400

    Comments

--- src/common/Team.java
+++ src/common/Team.java
@@ -1,11 +1,14 @@
+/**
+ * Team
+ * Version 1.0
+ * @author Charles Wong
+ * 2020-06-21
+ */
+
+//package statement
 package common;
 
-/**
- * 
- * @author Charles Wong
- *
- */
 public enum Team {
 	WHITE,
 	BLACK
-}
+}//end of Team Class
