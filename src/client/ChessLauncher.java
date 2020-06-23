43eb0ecede30126fa9d66128b83a5b1175ca2caf
TLiu-8 <66648833+TLiu-8@users.noreply.github.com>
2020-06-22 20:17:12 -0400

    Add files via upload

---
+++ src/client/ChessLauncher.java
@@ -0,0 +1,17 @@
+/**
+ * ChessLauncher
+ * Version 1.0
+ * Author: Theo Liu
+ * Date: 2020-06-22
+ * Description: Launches the Chess Game
+ */
+
+//package statements
+package client;
+
+class ChessLauncher{
+    public static void main(String[] args){
+        new ChessMenu();
+    }
+}
+//end of Chess Launcher
