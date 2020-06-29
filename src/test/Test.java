4fc0ddcdf79a7060f201f18f4abc600c99a08a2a
Charles <charleswonghm@gmail.com>
2020-06-29 18:25:49 -0400

    organized

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,4 +1,4 @@
-package common;
+package test;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -8,6 +8,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import client.ClientBoard;
+import common.Team;
 
 public class Test extends JFrame {
 
--- src/test/TestServer.java
+++ src/test/TestServer.java
@@ -1,4 +1,4 @@
-package common;
+package test;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
